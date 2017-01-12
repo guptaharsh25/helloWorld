c155d4b00c13738f41ae3a673cfc2511f560aca4
guptaharsh25 <reyharsh25@gmail.com>
2017-01-12 00:15:58 -0800

    Added background and button
    Placed button on top of background in storyboard.

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Harsh Gupta on 2017-01-11.
+//  Copyright © 2017 Harsh Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
